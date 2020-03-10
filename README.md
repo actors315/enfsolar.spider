a5484f3916cb062ebad54c3cca64a9ec4d15ce73
xiehuanjin <xiehuanjin@globalegrow.com>
2020-03-10 10:03:21 +0800

    company list

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # enfsolar.spider
+
+这是一个简单的爬虫，是个学习实践项目
+
+* Python3
+* urllib
+* re
+* json
+* csv
+* xlsxwriter
--- directory.py
+++ directory.py
@@ -7,7 +7,7 @@ class Handler:
 
     def __init__(self):
         self.baseURL = u"https://www.enfsolar.com/"
-        self.directoryUrl = '<a href="/directory/(.*)" class="no-hover-underline">'
+        self.directoryUrl = '<a href="/directory/(.*?)" class="no-hover-underline">'
 
     def fetch_content(self, uri):
         url = self.baseURL + uri
@@ -29,7 +29,7 @@ class Spider:
 
     def run(self):
         directory_list = ['panel', 'component', 'material', 'equipment', 'seller', 'installer', 'application', 'service']
-        dir_url_list = [];
+        dir_url_list = []
         for directory in directory_list:
             temp_list = self.handler.get_directory_list(directory)
             dir_url_list.extend(temp_list)
