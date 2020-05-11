fa20b2a186ca53a92c3d4180af9729c4447362f4
actors315 <actors315@gmail.com>
2020-05-12 00:35:32 +0800

    test

---
+++ try_index.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+try_index=$(cat .travis.log)
+
+if try_index < 20 then
+  export AUTO_TRAVIS_RUN="travis ${try_index}st"
+else
+  export AUTO_TRAVIS_RUN="skip travis"
+fi
+
+try_index=try_index+1
+
+echo try_index > .travis.log
